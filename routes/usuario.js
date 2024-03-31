da1855fb2c57f809fe56f8c59b94aa458d5be344
joseluismadrid <joseluis6112011@hotmail.com>
2024-03-31 18:25:53 -0500

    segundo
    Signed-off-by: joseluismadrid <joseluis6112011@hotmail.com>

--- routes/usuario.js
+++ routes/usuario.js
@@ -10,23 +10,11 @@ const{getUsuario, postUsuario, putUsuario,deleteUsuario} = require('../controlle
 
 
 route.get('/', getUsuario),
-route.post('/', postUsuario)
-route.put('/', putUsuario)
-route.delete('/', deleteUsuario)
+route.post('/', postUsuario),
+route.put('/', putUsuario),
+route.delete('/', deleteUsuario),
 
-//Consultar un dato
-// route.get('/consultarUsuario', (req, res) => {
-//     res.json({
-//         msg: 'Lista Datos'
-//     })
-// })
 
-//Metodo para agregar datos
-// route.post('/', (req, res) => {
-//     res.json({
-//         msg: 'Insercion exitosa'
-//     })
-// })
 
 
 module.exports = route 
